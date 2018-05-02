3a0639e63bfabce0e4b19c89bc64546a6ab44f2d
mhoshino <35358706+mhoshino@users.noreply.github.com>
2018-05-02 13:21:46 +0900

    Update install_salt_master.sh

--- install_salt_master.sh
+++ install_salt_master.sh
@@ -14,23 +14,30 @@ HOSTNAME_IP=`hostname -I | awk '{print $1}'`
 
 # Add domain to etc_hosts if not configured 
 hostname -f | grep -q "\." ||  {
-  sed '/$HOSTNAME_IP/d' /etc/hosts
+  sed -i '/$HOSTNAME_IP/d' /etc/hosts
   echo "$HOSTNAME_IP $HOSTNAME $HOSTNAME.local" >> /etc/hosts
 }
 
 # Install software
+apt-get update
 apt-get install --no-install-recommends -y git
 apt-get install git vim tree python-pip -y
 pip install --upgrade pip
 
 # Add salt repository
-apt-cache policy | grep -qw http://repo.saltstack.com/apt/ubuntu/$UBUNTUVERSION/amd64/$SALTVERSION || {
-  wget -O - https://repo.saltstack.com/apt/ubuntu/$UBUNTUVERSION/amd64/$SALTVERSION/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-  cat > /etc/apt/sources.list.d/saltstack.list << EOF
+wget -O - https://repo.saltstack.com/apt/ubuntu/$UBUNTUVERSION/amd64/$SALTVERSION/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
+cat > /etc/apt/sources.list.d/saltstack.list << EOF
 deb http://repo.saltstack.com/apt/ubuntu/$UBUNTUVERSION/amd64/$SALTVERSION $UBUNTUCODE main
 EOF
   apt-get update  
 }
+
+# Install salt_minion
+apt-get install salt-common salt-minion -y
+
+cat > /etc/salt/minion.d/minion.conf << EOF
+master: HOSTNAME_IP
+EOF
 
 # Install and config(temporary) salt master
 apt-get install reclass salt-master -y
