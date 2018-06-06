9f99356b284caab588023245002cbcf0d63e1cd2
mhoshino <mhoshino@mirantis.com>
2018-06-06 03:51:00 +0000

    add prx

--- heat_templates/common.sh
+++ heat_templates/common.sh
@@ -2,9 +2,9 @@
 sh -x
 if [ `hostname` = "stacknamecfg_hostname" ]
 then
-  curl -O https://raw.githubusercontent.com/mhoshino/simplemcp/master/install_salt_master.sh
+  curl -O https://raw.githubusercontent.com/mirantisjp/simplemcp/master/install_salt_master.sh
   sh -x ./install_salt_master.sh stackname ubuntu_version ubuntu_code salt_version system_class_version
 else
-  curl -O https://raw.githubusercontent.com/mhoshino/simplemcp/master/install_salt_minion.sh
+  curl -O https://raw.githubusercontent.com/mirantisjp/simplemcp/master/install_salt_minion.sh
   sh -x ./install_salt_minion.sh cfg_ip ubuntu_version ubuntu_code salt_version
 fi
