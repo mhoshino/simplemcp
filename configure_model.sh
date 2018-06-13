cb127d99932653d0f46f9884342367a6b9418173
mhoshino <mhoshino@mirantis.com>
2018-06-13 02:58:01 +0000

    add configure sh

---
+++ configure_model.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+CLUSTERNAME=$1
+DEPLOY_NAME=$2
+
+git clone https://github.com/mirantisjp/simplemcp
+ln -s /simplemcp/common /srv/salt/reclass/classes/cluster/common
+if [[ -n DEPLOY_NAME ]]
+then
+  ln -s /simplemcp/$DEPLOY_NAME /srv/salt/reclass/classes/cluster/$CLUSTERNAME
+  salt-call debug state.apply
+  salt -C "*" state.apply linux,salt
+  salt -C "*" saltutil.sync_all
+fi
+
