4b42193dbf9ed79b73572ce69b9608cea3170b57
mhoshino <35358706+mhoshino@users.noreply.github.com>
2018-06-13 12:17:07 +0900

    Update configure_model.sh

--- configure_model.sh
+++ configure_model.sh
@@ -6,7 +6,7 @@ DEPLOY_NAME=$2
 
 git clone https://github.com/mirantisjp/simplemcp
 ln -s /simplemcp/common /srv/salt/reclass/classes/cluster/common
-if [[ -n DEPLOY_NAME ]]
+if [ -n DEPLOY_NAME ]
 then
   ln -s /simplemcp/$DEPLOY_NAME /srv/salt/reclass/classes/cluster/$CLUSTERNAME
   salt-call state.apply
