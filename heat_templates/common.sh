97aa4252f294e33a80441655d64f87ccae5feccc
mhoshino <mhoshino@mirantis.com>
2018-06-14 06:00:26 +0000

    fix

--- heat_templates/common.sh
+++ heat_templates/common.sh
@@ -4,6 +4,8 @@ if [ `hostname` = "stacknamecfg_hostname" ]
 then
   curl -O https://raw.githubusercontent.com/mirantisjp/simplemcp/master/install_salt_master.sh
   sh -x ./install_salt_master.sh stackname ubuntu_version ubuntu_code salt_version system_class_version
+  curl -O https://raw.githubusercontent.com/mirantisjp/simplemcp/master/configure_model.sh
+  sh -x ./configure_model.sh stackname deploy_name
 else
   curl -O https://raw.githubusercontent.com/mirantisjp/simplemcp/master/install_salt_minion.sh
   sh -x ./install_salt_minion.sh cfg_ip ubuntu_version ubuntu_code salt_version
